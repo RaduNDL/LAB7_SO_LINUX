89d50f1e868c5c1c24b9b232cd71d9f48778d35a
Radu <164653790+RaduNDL@users.noreply.github.com>
2024-12-11 20:12:40 +0000

    Update LAB7_SO.cpp

--- LAB7_SO.cpp
+++ LAB7_SO.cpp
@@ -1,77 +1,96 @@
 #include <iostream>
-#include <thread>
 #include <semaphore.h>
-#include <string>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <cstring>
 
-// Semafoare globale pentru sincronizare
-sem_t mutex;
-sem_t whiteQueue;
-sem_t blackQueue;
-bool lastWasWhite = false;
+// Variabile globale partajate între procese (utilizând mmap)
+bool *lastWasWhite;
+sem_t *mutex;
+sem_t *whiteQueue;
+sem_t *blackQueue;
 
 // Funcția care simulează procesul alb
 void runWhiteProcess() {
     std::cout << "Procesul alb rulează...\n";
-    sleep(1);  // Simulează timp de execuție
+    sleep(1); // Simulează timp de execuție
 }
 
 // Funcția care simulează procesul negru
 void runBlackProcess() {
     std::cout << "Procesul negru rulează...\n";
-    sleep(1);  // Simulează timp de execuție
+    sleep(1); // Simulează timp de execuție
 }
 
-// Funcția de accesare a resursei de către fire albe și negre
-void accessResource(const std::string &threadType) {
-    if (threadType == "white") {
-        sem_wait(&whiteQueue);
-        if (!lastWasWhite) {
-            sem_wait(&mutex);
+// Funcția de accesare a resursei de către procese albe și negre
+void accessResource(const std::string &processType) {
+    if (processType == "white") {
+        sem_wait(whiteQueue);
+        if (!*lastWasWhite) {
+            sem_wait(mutex);
         }
-        std::cout << "Firul alb accesează resursa.\n";
-        runWhiteProcess();  // Simulează rularea procesului alb
-        std::cout << "Firul alb a terminat utilizarea resursei.\n";
-        lastWasWhite = true;
-        sem_post(&mutex);
-        sem_post(&whiteQueue);
-    } else if (threadType == "black") {
-        sem_wait(&blackQueue);
-        if (lastWasWhite) {
-            sem_wait(&mutex);
+        std::cout << "Procesul alb accesează resursa.\n";
+        runWhiteProcess();
+        std::cout << "Procesul alb a terminat utilizarea resursei.\n";
+        *lastWasWhite = true;
+        sem_post(mutex);
+        sem_post(whiteQueue);
+    } else if (processType == "black") {
+        sem_wait(blackQueue);
+        if (*lastWasWhite) {
+            sem_wait(mutex);
         }
-        std::cout << "Firul negru accesează resursa.\n";
-        runBlackProcess();  // Simulează rularea procesului negru
-        std::cout << "Firul negru a terminat utilizarea resursei.\n";
-        lastWasWhite = false;
-        sem_post(&mutex);
-        sem_post(&blackQueue);
+        std::cout << "Procesul negru accesează resursa.\n";
+        runBlackProcess();
+        std::cout << "Procesul negru a terminat utilizarea resursei.\n";
+        *lastWasWhite = false;
+        sem_post(mutex);
+        sem_post(blackQueue);
     }
 }
 
 int main() {
+    // Alocare memorie partajată pentru variabile globale
+    lastWasWhite = static_cast<bool *>(mmap(NULL, sizeof(bool), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    mutex = static_cast<sem_t *>(mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    whiteQueue = static_cast<sem_t *>(mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    blackQueue = static_cast<sem_t *>(mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+
     // Inițializare semafoare
-    sem_init(&mutex, 0, 1);
-    sem_init(&whiteQueue, 0, 1);
-    sem_init(&blackQueue, 0, 1);
+    sem_init(mutex, 1, 1);
+    sem_init(whiteQueue, 1, 1);
+    sem_init(blackQueue, 1, 1);
+    *lastWasWhite = false;
 
-    // Crearea firelor de execuție
-    std::thread whiteThread1([] { accessResource("white"); });
-    std::thread whiteThread2([] { accessResource("white"); });
-    std::thread blackThread1([] { accessResource("black"); });
-    std::thread blackThread2([] { accessResource("black"); });
+    pid_t pids[4];
 
-    // Așteptarea firelor
-    whiteThread1.join();
-    whiteThread2.join();
-    blackThread1.join();
-    blackThread2.join();
+    // Crearea proceselor albe și negre
+    for (int i = 0; i < 4; i++) {
+        pids[i] = fork();
+        if (pids[i] == 0) {
+            if (i % 2 == 0) {
+                accessResource("white");
+            } else {
+                accessResource("black");
+            }
+            _exit(0); // Termină procesul copil
+        }
+    }
 
-    // Distrugerea semafoarelor
-    sem_destroy(&mutex);
-    sem_destroy(&whiteQueue);
-    sem_destroy(&blackQueue);
+    // Așteaptă toate procesele copil să termine
+    for (int i = 0; i < 4; i++) {
+        waitpid(pids[i], NULL, 0);
+    }
+
+    // Distrugere semafoare și eliberare memorie partajată
+    sem_destroy(mutex);
+    sem_destroy(whiteQueue);
+    sem_destroy(blackQueue);
+    munmap(lastWasWhite, sizeof(bool));
+    munmap(mutex, sizeof(sem_t));
+    munmap(whiteQueue, sizeof(sem_t));
+    munmap(blackQueue, sizeof(sem_t));
 
     return 0;
 }
-
